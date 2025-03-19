a0c64e8b20f36d793c81a0b6fe0c8cbd420e155d
SiddharthSundram <155897849+SiddharthSundram@users.noreply.github.com>
2025-03-19 18:04:17 +0530

    update in sets

--- 2Basic DataStructure/3learnSet.py
+++ 2Basic DataStructure/3learnSet.py
@@ -7,3 +7,88 @@
 # print(set())# creating empty set
 # print(1 in set1) # way to check that element 1 is in set1 or not
 # print(9 in set2) # way to check that element 9 is in set2 or not
+
+
+# lang = {'c','c++','python','java','js','c++'}
+# print(lang,type(lang),len(lang),id(lang))
+# print()
+
+
+# #forming a set form tuple
+# lang = set(('c','c++','python','java','js','c++'))
+# print(lang,type(lang),len(lang),id(lang))
+# print()
+
+# #formin a set
+# lang = {'c','c++','python','java','js','c++'}
+# print(lang,type(lang),len(lang),id(lang))
+# print()
+
+
+# substraction 
+# lang = {'c','c++','python'}
+# print(lang,type(lang),len(lang),id(lang))
+
+# snack = {'wiper','python','cobra'}
+# print(snack,type(snack),len(snack),id(snack))
+
+# result = lang.difference(snack)
+# print(result,type(result),len(result),id(result))
+
+# result = lang - snack
+# print(result,type(result),len(result),id(result))
+
+
+
+# lang.difference_update(snack)
+# print(lang,type(lang),len(lang),id(lang))
+# print(snack,type(snack),len(snack),id(snack))
+
+
+
+# # symetric diff
+# lang = {'c','c++','python'}
+# print(lang,type(lang),len(lang),id(lang))
+
+# snack = {'wiper','python','cobra'}
+# print(snack,type(snack),len(snack),id(snack))
+
+# result = lang.symmetric_difference(snack)
+# print(result,type(result),len(result),id(result))
+
+
+# result = lang ^ snack
+# print(result,type(result),len(result),id(result))
+
+
+# # intersection update
+# lang = {'c','c++','python'}
+# print(lang,type(lang),len(lang),id(lang))
+
+# snack = {'wiper','python','cobra'}
+# print(snack,type(snack),len(snack),id(snack))
+
+# result = lang.intersection(snack)
+# print(result,type(result),len(result),id(result))
+
+
+# result = lang & snack  # & sumbol of inteersction
+# print(result,type(result),len(result),id(result))
+
+# lang.intersection_update(snack)
+# print(lang,type(lang),len(lang),id(lang))
+
+
+
+# lang = {'c','c++','python'}
+# print(lang,type(lang),len(lang),id(lang))
+
+# snack = {'wiper','python','cobra'}
+# print(snack,type(snack),len(snack),id(snack))
+
+# result = lang.union(snack)
+# print(result,type(result),len(result),id(result))
+
+
+# result = lang |snack  # | sumbol of union
+# print(result,type(result),len(result),id(result))
